4591d418cb30408c5427955e0a3c05ea4138c882
Axel Navarro <anavarro@ironsoft.com.ar>
2013-10-24 16:32:49 -0300

    +MVW angular model

---
+++ public/javascripts/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('sales', [
+  'sales.controllers',
+  'sales.filters',
+  'sales.services',
+  'sales.directives'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/view1', {
+      templateUrl: 'partials/partial1',
+      controller: 'MyCtrl1'
+    }).
+    when('/view2', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    otherwise({
+      redirectTo: '/view1'
+    });
+
+  $locationProvider.html5Mode(true);
+});
