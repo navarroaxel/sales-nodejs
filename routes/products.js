a177df8a486b9b8965770b2aa1276259d79f1429
jpogonza <jpogonza@ironsoft.com.ar>
2013-10-30 12:44:43 -0300

    Paleta

--- routes/products.js
+++ routes/products.js
@@ -28,10 +28,10 @@ exports.create = function(req, res, next) {
 exports.update = function(req, res, next) {
 	Product.findById(req.params.id, function(err, product) {
 		if (err) return next(err);
-		Product.name = req.body.name;
-		Product.stock = req.body.stock;
-		Product.price = req.body.price;
-		Product.save();
+		product.name = req.body.name;
+		product.stock = req.body.stock;
+		product.price = req.body.price;
+		product.save();
 		res.end();
 	});
 }
