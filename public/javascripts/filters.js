f6c52a4e4dc0c1bbfca266b2b6d6e5feafba222c
Axel Navarro <anavarro@ironsoft.com.ar>
2013-11-15 16:39:33 -0300

    +purchase status enum filter in AngularJS

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -5,4 +5,20 @@ angular.module('sales.filters', []).
     return function (input, searchRegex, replaceRegex) {
       return input.replace(RegExp(searchRegex), replaceRegex);
     }
+  }).
+  filter('purchaseStatus', function(){
+  	return function(input){
+  		switch(input){
+  			case 'in_progress':
+				return 'In progress';
+			case 'delivered':
+				return 'Delivered';
+			case 'payed':
+				return 'Payed';
+			case 'closed':
+				return 'Closed';
+			default:
+				return input;
+  		}
+  	}
   });
